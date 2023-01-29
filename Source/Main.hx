12f0b7b678e6f85ddcd86146a55552f117059924
EliteMasterEric <ericmyllyoja@gmail.com>
2023-01-29 16:00:37 -0500

    Cleanup

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,68 +1,54 @@
 package;
 
-
-import hx.concurrent.lock.RLock;
 import lime.app.Application;
 import lime.graphics.RenderContext;
 
+/**
+ * The entry point of the application.
+ */
+class Main extends Application
+{
+  public function new()
+  {
+    super();
 
-class Main extends Application {
-	
-  var lock:RLock;
+    trace('Hello World');
+  }
 
-	public function new () {
-		
-		super ();
-		
-		trace ("Hello World");
-		
-    lock = new RLock();
-	}
-	
-	
-	public override function render (context:RenderContext):Void {
-		
-		switch (context.type) {
-			
-			case CAIRO:
-				
-				var cairo = context.cairo;
-				
-				cairo.setSourceRGB (0.75, 1, 0);
-				cairo.paint ();
-			
-			case CANVAS:
-				
-				var ctx = context.canvas2D;
-				
-				ctx.fillStyle = "#BFFF00";
-				ctx.fillRect (0, 0, window.width, window.height);
-			
-			case DOM:
-				
-				var element = context.dom;
-				
-				element.style.backgroundColor = "#BFFF00";
-			
-			case FLASH:
-				
-				var sprite = context.flash;
-				
-				sprite.graphics.beginFill (0xBFFF00);
-				sprite.graphics.drawRect (0, 0, window.width, window.height);
-			
-			case OPENGL, OPENGLES, WEBGL:
-				
-				var gl = context.webgl;
-				
-				gl.clearColor (0.75, 1, 0, 1);
-				gl.clear (gl.COLOR_BUFFER_BIT);
-			
-			default:
-			
-		}
-		
-	}
-	
-	
+  public override function render(context:RenderContext):Void
+  {
+    switch (context.type)
+    {
+      case CAIRO:
+        var cairo:lime.graphics.CairoRenderContext = context.cairo;
+
+        cairo.setSourceRGB(0.75, 1, 0);
+        cairo.paint();
+
+      case CANVAS:
+        var ctx:lime.graphics.Canvas2DRenderContext = context.canvas2D;
+
+        ctx.fillStyle = '#BFFF00';
+        ctx.fillRect(0, 0, window.width, window.height);
+
+      case DOM:
+        var element:lime.graphics.DOMRenderContext = context.dom;
+
+        element.style.backgroundColor = '#BFFF00';
+
+      case FLASH:
+        var sprite:lime.graphics.FlashRenderContext = context.flash;
+
+        sprite.graphics.beginFill(0xBFFF00);
+        sprite.graphics.drawRect(0, 0, window.width, window.height);
+
+      case OPENGL, OPENGLES, WEBGL:
+        var gl:lime.graphics.WebGLRenderContext = context.webgl;
+
+        gl.clearColor(0.75, 1, 0, 1);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+      default:
+    }
+  }
 }
